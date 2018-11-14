2ba7f541d418afbd01a1c4ee0210fbb8ae2a3365
Ryan LaRose <rlarose@umich.edu>
2018-11-14 11:10:02 -0500

    Updates to PauliMatrix class. Now has method to compute matrix.

---
+++ pauli/run_vls.py
@@ -0,0 +1,36 @@
+"""
+Main script for running vls with pauli operators.
+
+written by Ryan LaRose <rlarose@umich.edu>
+at LANL 11-14-2018
+"""
+
+# =============================================================================
+# imports
+# =============================================================================
+
+import numpy as np
+
+import cirq
+
+from vls_pauli import PauliMatrix
+
+# =============================================================================
+# constants
+# =============================================================================
+
+# pauli operators making up A. each row corresponds to a term of paulis
+Amat_ops = np.array([["X", "Z", "Z", "Y"],
+                     ["Y", "I", "X", "Z"],
+                     ["Z", "X", "Y", "Z"]])
+
+# coefficients multiplying the terms of the pauli operators in A
+Amat_coeffs = np.array([1. +0.3j, -0.4 - 1j, 2. + 4.2j])
+
+# =============================================================================
+# main script
+# =============================================================================
+
+A = PauliMatrix(Amat_coeffs, Amat_ops)
+
+print(A.matrix())
--- pauli/vls_pauli.py
+++ pauli/vls_pauli.py
@@ -1,5 +1,6 @@
 """
-Code for running VLS where the matrix A is assumed to be a sum of paulis.
+Classes and functions for running VLS where the matrix A is assumed to be a 
+sum of pauli operators.
 
 written by Ryan LaRose <rlarose@umich.edu>
 at LANL 11-14-2018
@@ -14,24 +15,45 @@ import numpy as np
 import cirq
 
 # =============================================================================
-# constants
+# classes
 # =============================================================================
 
-Amat_ops = np.array([["X", "Z", "Z", "I"],
-                     ["I", "I", "X", "Z"],
-                     ["Z", "I", "I", "Z"]])
-
-Amat_coeffs = np.array([1., -1, 2.])
-
 class PauliMatrix():
+    """PauliMatrix class.
+    
+    """
+    # =========================================================================
+    # matrices of pauli operators
+    # =========================================================================
+    
+    # identity
+    imat = np.array([[1., 0.],
+                     [0., 1.]], dtype=np.complex64)
+
+    # pauli x
+    xmat = np.array([[0., 1.],
+                     [1., 0.]], dtype=np.complex64)
+
+    # pauli y
+    ymat = np.array([[0., -1j],
+                     [1j, 0.]], dtype=np.complex64)
+
+    # pauli z
+    zmat = np.array([[1., 0.],
+                     [0., -1.]], dtype=np.complex64)
+
+    # =========================================================================
+    # matrices of pauli operators
+    # =========================================================================
+
     def __init__(self, coeffs, ops):
         # check inputs
         assert len(coeffs) == ops.shape[0]
-        
+
         # attributes
         self.coeffs = coeffs
         self.ops = ops
-    
+
     def num_qubits(self):
         """Returns the number of qubits the PauliMatrix acts on."""
         return self.ops.shape[1]
@@ -47,5 +69,37 @@ class PauliMatrix():
     
     def matrix(self):
         """Returns a matrix representation of the PauliMatrix."""
-        pass
+        # allocate space
+        mat = np.zeros(self.size(), dtype=np.complex64)
+        
+        # loop over all terms in operators
+        for (ind, op_list) in enumerate(self.ops):
+            
+            # form the first kronecker product
+            if len(op_list) == 1:
+                term = self._key_to_mat(op_list[0])
+            else:
+                term = np.kron(self._key_to_mat(op_list[0]),
+                               self._key_to_mat(op_list[1]))
+            
+            # form all other terms in kronecker product
+            for k in range(2, len(op_list)):
+                key = op_list[k]
+                term = np.kron(term, self._key_to_mat(key))
+            
+            # add the term to the matrix
+            mat += self.coeffs[ind] * term
+            
+        return mat
+            
+            
+    def _key_to_mat(self, key:str):
+        """Returns a pauli matrix corresponding to a string key."""
+        key_mat = {"I": self.imat,
+                   "X": self.xmat,
+                   "Y": self.ymat,
+                   "Z": self.zmat}
+        
+        return key_mat[key]
+            
 
