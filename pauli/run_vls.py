Fixed bug in computing the normalization factor for the cost computation in PauliSystem.

pauli/run_vls.py
@@ -20,16 +20,16 @@ from vls_pauli import PauliSystem
 # =============================================================================
 
 # pauli operators making up A. each row corresponds to a term of paulis
-#Amat_ops = np.array([["X", "Z", "Z", "Y"],
-#                     ["Y", "I", "X", "Z"],
-#                     ["Z", "X", "Y", "Y"]])
+Amat_ops = np.array([["X", "Z", "Z", "Y"],
+                     ["Y", "I", "X", "Z"],
+                     ["Z", "X", "Y", "Y"]])
 
-Amat_ops = np.array([["X", "Z", "Z", "Y"]])
+#Amat_ops = np.array([["X", "Z", "Z", "Y"]])
 
 # coefficients multiplying the terms of the pauli operators in A
-#Amat_coeffs = np.array([1. +0.3j, -0.4 - 1j, 2. + 4.2j])
+Amat_coeffs = np.array([1. +0.3j, -0.4 - 1j, 2. + 4.2j])
 
-Amat_coeffs = np.array([0.5 - 0.5j])
+#Amat_coeffs = np.array([0.5 - 0.5j])
 
 bvec_ops = np.array(["X", "X", "X", "X"])
 
@@ -38,7 +38,7 @@ bvec_ops = np.array(["X", "X", "X", "X"])
 # =============================================================================
 
 # set a random seed
-np.random.seed(seed=100)
+#np.random.seed(seed=100)
 
 # get a pauli system
 system = PauliSystem(Amat_coeffs, Amat_ops, bvec_ops)
@@ -53,8 +53,9 @@ print(system.make_hadamard_test_circuit(
         system.ops[0], system.ops[0], 2, "real")
     )
 
+system.make_ansatz_circuit()
 # set some angles
-angles = np.zeros(48)
+angles = np.random.randn(48)
 
 # compute each expectation and time it
 start = time()

pauli/vls_pauli.py
@@ -702,27 +702,31 @@ class PauliSystem():
         conj = lambda z: np.complex.conjugate(z)
         
         # compute the each term in the local cost function
+        norm = 0.0
         for k in range(n_terms):
             for l in range(k, n_terms):
                 # variable to store the sum over j term
-                jterm = 0.
+                jterm = 0.0
                 # loop over all "j values" in cost function definition
-                for j in range(1, n):
+                for j in range(n):
                     # run the hadamard test to compute get the real part
                     jterm += self.run_hadamard_test(
                         angles, self.ops[k], self.ops[l], j, "real"
                         )
-                # divide the jterm by the norm
-                nterm = self.run_norm_circuit(angles, self.ops[k], self.ops[l], "real")
-                jterm = jterm / nterm
-                
+                # add to the norm term
+                norm += self.run_norm_circuit(angles, self.ops[k], self.ops[l], "real")
+                print("jterm =", jterm)
                 # add the appropriate factors
                 if k == l:
                     cval += self.coeffs[k] * conj(self.coeffs[l]) * jterm
                 else:
                     cval += 2. * np.real(self.coeffs[k] * conj(self.coeffs[l])) * jterm
+                print("CVAL = ", cval)
+        print("norm =", norm)
+        print("cval =", cval)
+        print("n =", n)
         # return the real part to avoid numerical error/small imaginary parts
-        cost = np.real(1. - cval / n)
+        cost = np.real(1. - cval / n / norm)
         print(cost)
         return cost
     